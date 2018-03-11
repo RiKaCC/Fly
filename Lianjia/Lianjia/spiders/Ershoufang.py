d279d7bd0f01df5155d9bdee1e04b5f90614e06a
RikaCC <chenruijia_cc@163.com>
2018-03-11 18:06:48 +0800

    do some modify

--- Lianjia/Lianjia/spiders/Ershoufang.py
+++ Lianjia/Lianjia/spiders/Ershoufang.py
@@ -2,6 +2,7 @@
 import scrapy
 import pymysql
 import time
+import re
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 
@@ -39,9 +40,19 @@ class ErshoufangSpider(scrapy.Spider):
         ret = {}
         for i, val in enumerate(torrent['name']):
             ret[val] = torrent['price'][i]
-            sql = "INSERT INTO ErshoufangBasic(houseid,name,price,follow,watch,querytime) VALUES(%s,%s,%s,%s,%s,%s)"
+            
+            sql = "INSERT INTO ErshoufangBasic(houseid,name,price,follow,watch,querytime) VALUES(%s,%s,%s,%s,%s,%s) on duplicate key update name=%s,price=%s,follow=%s,watch=%s, querytime=%s"#name=`torrent['name'][i]`,price=`torrent['price'][i]`,follow=`torrent['follow'][i]`,watch=`torrent['watch'][i]`,querytime=`now`"
+            follow = getNum(torrent['follow'][i])
+            watch = getNum(torrent['watch'][i])
             print(sql)
             print(torrent['houseid'][i], val, torrent['price'][i], torrent['follow'][i], torrent['watch'][i], now)
-            cursor.execute(sql,(torrent['houseid'][i], val, torrent['price'][i], torrent['follow'][i], torrent['watch'][i], now))
-        #print(ret)
+            cursor.execute(sql,(torrent['houseid'][i], val, torrent['price'][i], follow, torrent['watch'][i], now, val,torrent['price'][i],follow,watch, now))
+            #cursor.execute(sql)
+           #print(ret)
         return torrent
+
+def getNum(strs):
+    print(re.sub("\D", "", strs))
+    return re.sub("\D", "", strs)
+
+
